855ffd502bda03d33488ff86006806da330f253a
Anton Jackson-Smith <anton@bnext.bio>
2025-07-17 20:29:39 -0700

    Initial dotfiles commit for Mac command line usage

---
+++ run_after_conda_init.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+if [ -f /opt/miniconda3/bin/conda ]; then
+  /opt/miniconda3/bin/conda init zsh
+fi
---
+++ run_onchange_before_brew.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# Basics
+brew install btop
+brew install git
+brew install eza
+brew install mosh
+brew install fzf
+brew install zoxide
+
+# Fonts
+brew install --cask font-iosevka
+brew install --cask font-iosevka-nerd-font
+
+# Dev
+brew install poetry
+brew install uv
